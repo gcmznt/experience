b54debdfd32ca703e386aed93b1f4e40357e1360
Giacomo Zinetti <giacomo.zinetti@objectway.it>
2015-07-06 11:04:50 +0200

    pubsub

---
+++ js/pubsub.js
@@ -0,0 +1,41 @@
+var events = (function(){
+  var topics = {};
+  var hOP = topics.hasOwnProperty;
+
+  return {
+    subscribe: function(topic, listener) {
+      // Create the topic's object if not yet created
+      if(!hOP.call(topics, topic)) topics[topic] = [];
+
+      // Add the listener to queue
+      var index = topics[topic].push(listener) -1;
+
+      // Provide handle back for removal of topic
+      return {
+        remove: function() {
+          delete topics[topic][index];
+        }
+      };
+    },
+    publish: function(topic, info) {
+      // If the topic doesn't exist, or there's no listeners in queue, just leave
+      if(!hOP.call(topics, topic)) return;
+
+      // Cycle through topics queue, fire!
+      topics[topic].forEach(function(item) {
+      		item(info != undefined ? info : {});
+      });
+    }
+  };
+})();
+
+
+
+events.publish('/page/load', {
+	url: '/some/url/path' // any argument
+});
+
+var subscription = events.subscribe('/page/load', function(obj) {
+	// Do something now that the event has occurred
+});
+subscription.remove();
